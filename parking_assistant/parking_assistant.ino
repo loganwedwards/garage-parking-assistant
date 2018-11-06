30243c266462ca852b25ec67fb7267bb0ed67e90
Logan Edwards <logan.edwards@fountstudio.com>
2018-11-06 12:55:02 -0500

    Save setpoint to memory. Add some debugging

--- parking_assistant/parking_assistant.ino
+++ parking_assistant/parking_assistant.ino
@@ -1,13 +1,14 @@
 #include <Adafruit_NeoPixel.h>
+#include <EEPROM.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-#define PIN 3	 // input pin Neopixel is attached to, (D7)
+#define ledPin 3	 // input pin Neopixel is attached to, (D7)
 #define NUMPIXELS 8 // number of neopixels in Ring
 #define pingPin 4 // Trigger Pin
 #define buttonPin 6 // momentary button for training mode
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, ledPin, NEO_GRB + NEO_KHZ800);
 
 int delayval = 100; // timing delay
 
@@ -17,21 +18,33 @@ int greenColor = 0;
 int blueColor = 0;
 int buttonState = 0;
 long setpoint = 50; // distance in cm
-int maximumRange = 350;                                                                                                                                                                                                                                                            ; // Maximum range needed
-int minimumRange = 5; // Minimum range needed
+long memorySetpoint;
+int maximumRange = 280; // PING))) Max Detection = 300cm                                                                                                                                                                                                                                                            ; // Maximum range needed
+int minimumRange = 3; // PING))) Min Detection = 2cm
 long duration, distance; // Duration used to calculate distance
 
 // Smoothing and timer for idle operation
 const int numReadings = 4;
+const int setpointAddress = 0; // EEPROM memory address
 long readings[numReadings];
 int readIndex = 0;
 long total = 0;
 float average = 0.0;
 
 void setup() {
+  Serial.begin(9600);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
-
+  // Enable for debugging
+  while (!Serial) {
+    ;
+  }
+  EEPROM.get(setpointAddress, memorySetpoint);
+  Serial.print("Memory Setpoint ");
+  Serial.println(memorySetpoint);
+  if (memorySetpoint > 0) {
+    setpoint = memorySetpoint;
+  }
   // initialize all the readings to 0:
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     readings[thisReading] = 0;
@@ -39,26 +52,29 @@ void setup() {
   pixels.setBrightness(128);
   pixels.begin(); // Initializes the NeoPixel library.
   pixels.show();
-  Serial.begin(9600);
+  
   for(int i=0;i<NUMPIXELS;i++){
       // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
       pixels.setPixelColor(i,255, 255, 255); // white
       pixels.show(); // This sends the updated pixel color to the hardware.
       delay(50); // Delay for a period of time (in milliseconds).
-    distance = getDistance();
+    
     }
 }
 
 void loop() {
+  Serial.print("[Distance: ");
   Serial.print(distance);
-  Serial.print(" / ");
-  Serial.println(setpoint);
+  Serial.print(" ] / [ Setpoint: ");
+  Serial.print(setpoint);
+  Serial.println(" ]");
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
-//  Serial.print("Button state ");
-//  Serial.println(buttonState);
+  Serial.print("Button state ");
+  Serial.println(buttonState);
   if (buttonState == LOW) {
     // naive smoothing
+    // src: https://www.arduino.cc/en/Tutorial/Smoothing
     distance = getDistance();
     total = total - readings[readIndex];
     readings[readIndex] = distance;
@@ -75,8 +91,8 @@ void loop() {
     //  Serial.print(value);
     //  Serial.print("    Distance ");
     //  Serial.print(distance);
-    //  Serial.print("    Average ");
-    //  Serial.print(average);
+      Serial.print("    Average ");
+      Serial.println(average);
     //  Serial.print("    Variation ");
     //  Serial.println(variation);
 
@@ -87,8 +103,8 @@ void loop() {
       delay(500);
     } else {
       // normal operating mode
-    // Serial.println("Mode = Ready");
-//    Serial.println(distance);
+      // Serial.println("Mode = Ready");
+      // Serial.println(distance);
     if (distance <= setpoint) {
       setDanger(); 
     } else if (distance > maximumRange) {
@@ -96,7 +112,7 @@ void loop() {
     } else {
       // this represents the mapped index of the number of pixels
       // to show as yellow
-      int index = map(distance, minimumRange, maximumRange, 0, NUMPIXELS-1);
+      int index = map(average, minimumRange, maximumRange, 0, NUMPIXELS-1);
       setRange(index);
     }
     }
@@ -114,6 +130,7 @@ void loop() {
     }
     
     setpoint = distance;
+    EEPROM.put(setpointAddress, setpoint);
   }
 }
 
